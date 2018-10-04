e23a2dcefccd8d236c81e94b2a82d6cea495565f
adesurya16 <adesurya559@gmail.com>
2018-10-04 11:51:23 +0700

    model and db init

--- temporalaps/models.py
+++ temporalaps/models.py
@@ -5,27 +5,24 @@ from django.db import models
 class Client(models.Model):
     Nama = models.CharField(max_length = 255)
     Organisasi = models.CharField(max_length = 255)
-    Telp = models.CharField(max_length = 255)
+    Telepon = models.CharField(max_length = 255)
     Email = models.CharField(max_length = 255)
+    Valid_time_start = models.DateField(null = True)
+    Valid_time_end = models.DateField(null = True)
 
     def __str__(self):
         return self.Nama
 
 class Anggota(models.Model):
-    divisi = (
-        ('1', 'Manpro'),
-        ('2', 'Marketing'),
-        ('3', 'Teknikal')
-    )
     Nim = models.AutoField(primary_key = True)
     Nama = models.CharField(max_length = 255)
-    Alamat = models.CharField(max_length = 255)
-    Telepon = models.CharField(max_length = 255)
+    Alamat = models.CharField(max_length = 255, null=True)
+    Telepon = models.CharField(max_length = 255,  null=True)
     Email = models.CharField(max_length = 255)
-    Divisi = models.CharField(max_length = 1, choices=divisi)
+    Divisi = models.CharField(max_length = 255)
 
     def __str__(self):
-        return self.Nim
+        return self.Nama + " " + str(self.Nim) 
 
 class Proyek(models.Model):
     
